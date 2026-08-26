30a6f3b4e6e46b1817188d7928684ca1c68494a6
Clarissa <angie.ramos20137@gmail.com>
2026-08-26 09:16:46 -0600

    inicio proyecto progra 3

---
+++ Proyecto_Snake/gamewindow.h
@@ -0,0 +1,23 @@
+#ifndef GAMEWINDOW_H
+#define GAMEWINDOW_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class GameWindow;
+}
+QT_END_NAMESPACE
+
+class GameWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit GameWindow(QWidget *parent = nullptr);
+    ~GameWindow() override;
+
+private:
+    Ui::GameWindow *ui;
+};
+#endif // GAMEWINDOW_H
